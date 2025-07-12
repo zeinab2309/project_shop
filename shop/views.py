c82d0b2769f3a321fcf6bb0bbfdb323c4c1894ff
zeinab2309 <poormoosavie0@gmail.com>
2025-07-13 00:42:29 +0330

    start shop store

---
+++ shop/views.py
@@ -0,0 +1,32 @@
+from itertools import product
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render,get_object_or_404
+from .models import Category,Product
+
+# Create your views here.
+
+
+
+def product_list(request,category_slug=None):
+    category=None
+    categories=Category.objects.all()
+    products=Product.objects.all()
+    if category_slug:
+        category=get_object_or_404(Category,slug=category_slug)
+        products=products.filter(category=category)
+
+    context={
+        'category':category,
+        'categories':categories,
+        'products':products,
+    }
+    return  render(request,'shop/list.html',context)
+
+def product_detail(request,id,slug):
+    product=get_object_or_404(Product,slug=slug,id= id)
+    context = {
+        'product':product,
+    }
+    return render(request,'shop/detail.html',context)
+
