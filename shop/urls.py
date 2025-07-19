65abbe7d434b6e491daea72ad151986d07a85046
zeinab2309 <poormoosavie0@gmail.com>
2025-07-19 17:41:21 +0330

    final commit after cleanup

--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='shop'
 
 urlpatterns=[
-    path('products',views.product_list, name='products'),
+    path('products',views.product_list, name='product_list'),
     path('products/<slug:category_slug>/',views.product_list, name='product_list_by_category'),
     path('product/<int:id>/<slug:slug>/',views.product_detail, name='product_detail'),
 ]
