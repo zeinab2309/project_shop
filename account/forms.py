30ebe183294a0601a19e693c48de375066bc4669
zeinab2309 <poormoosavie0@gmail.com>
2025-07-30 21:14:41 +0330

    add Validation at forms.py

--- account/forms.py
+++ account/forms.py
@@ -8,9 +8,40 @@ class ShopUserCreationForm(UserCreationForm):
         model=ShopUser
         fields = ('phone','first_name','last_name','address','is_active','is_staff','is_superuser')
 
+    def clean_phone(self):
+        phone=self.cleaned_data.get('phone')
+        if self.instance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('Phone already exist')
+        else:
+            if ShopUser.objects.filter(phone=phone).exists():
+                raise forms.ValidationError('Phone already exist')
+        if not phone.isdigit():
+            raise forms.ValidationError('phone must be numbers')
+        if not phone.startswith('09'):
+            raise forms.ValidationError('phone must start whit 09 digits')
+        if len(phone)!=11:
+            raise forms.ValidationError('Phone must have 11 digit.')
+        return phone
 
 
 class ShopUserChangeForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model=ShopUser
         fields = ('phone','first_name','last_name','address','is_active','is_staff','is_superuser')
+
+    def clean_phone(self):
+        phone=self.cleaned_data.get('phone')
+        if self.instance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('Phone already exist')
+        else:
+            if ShopUser.objects.filter(phone=phone).exists():
+                raise forms.ValidationError('Phone already exist')
+        if not phone.isdigit():
+            raise forms.ValidationError('phone must be numbers')
+        if not phone.startswith('09'):
+            raise forms.ValidationError('phone must start whit 09 digits')
+        if len(phone)!=11:
+            raise forms.ValidationError('Phone must have 11 digit.')
+        return phone
