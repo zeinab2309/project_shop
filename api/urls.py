b1c8e701f20513adf670ddfd819edf4e3076fd2c
zeinab2309 <poormoosavie0@gmail.com>
2025-08-14 23:58:36 +0330

    added url path for api

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='api'
+
+urlpatterns=[
+    path('products/', views.ProductListAPIView.as_view(), name='products_list_api'),
+    path('product/<pk>',views.ProductDetailAPIView.as_view() ,name='products_detail_api'),
+
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from shop.models import Product
+from serializers import ProductSerializer
+from rest_framework import generics
 
-# Create your views here.
+#هدفمون اینه یک سری ابجکت های مدل پروداکت ک محصولات هست را بین دو تا وبسایت منتقل کنیم باید
+# برای انتقال از جیسون استفاده کنیم ک بیاد ابجکت های پیچیده مدل را به سریالایز های ساده تبدیل کنه
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
